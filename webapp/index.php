4d6d4b2124fc7854135faee2cfcf7b03f7806328
Michal <153352728+mickos7891@users.noreply.github.com>
2024-01-22 22:11:54 +0100

    PHP server and first page

---
+++ webapp/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+          <head>
+          <title>Black&Red Cinema</title>
+          <link rel="stylesheet" type="text/css" href="styles.css" />
+          </head>
+          <body>
+                    <header><a href="">Zaloguj</a> <a href="">Zarejestruj</a></header>
+                    <nav>
+                              <div class="navElements">
+                                        <div class="navElement">Repertuar</div>
+                                        <div class="navElement"><span>Kontakt</span></div>
+                                        <div class="navElement">O nas</div>
+                              </div>
+                    </nav>
+                    <footer>
+                              Miłego dnia
+                    </footer>
+          </body>
+          
+          
+</html>
