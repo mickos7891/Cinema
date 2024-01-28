cd4c08f87a9d479a3505287643492776b7a339ad
Michal <153352728+mickos7891@users.noreply.github.com>
2024-01-28 10:04:00 +0100

    Initialization fix

--- SQL/init.sql
+++ SQL/init.sql
@@ -1,4 +1,3 @@
-CREATE DATABASE CINEMA;
 USE CINEMA;
 
 CREATE TABLE SEANCES (
@@ -57,8 +56,12 @@ INSERT INTO CINEMA_HALLS (name, rows, seats_in_row) VALUES
 
 INSERT INTO MOVIES (title, minutes, description, image_url) VALUES
   ('Indiana Jones', 120, ' The movie follows the legendary archaeologist and adventurer, Dr. Indiana Jones, played by Harrison Ford, on a quest to find the Ark of the Covenant before the Nazis can obtain its supernatural powers. Set in the 1930s, the film is filled with thrilling action sequences, memorable characters, and a sense of humor.', 'images/image1.jpg'),
-  ('Star Wars', 105, 'Set in a distant galaxy, the story follows Luke Skywalker, played by Mark Hamill, a young farm boy who becomes embroiled in the Rebel Alliances fight against the tyrannical Galactic Empire. Assisted by Jedi Master Obi-Wan Kenobi, the charismatic smuggler Han Solo, and the droids C-3PO and R2-D2, Luke sets out on a journey to rescue Princess Leia, portrayed by Carrie Fisher, and deliver the plans to the Death Star, a powerful space station capable of destroying entire planets.', 'images/image3.jpg'),
-  ('Avengers', 95, 'The story follows the aftermath of "Avengers: Infinity War," where the villain Thanos successfully wiped out half of all life in the universe. In "Endgame," the surviving Avengers embark on a time-traveling mission to collect the Infinity Stones and undo Thanos devastating snap. The film explores themes of sacrifice, heroism, and the enduring strength of the human spirit.', 'images/image2.jpg');
+  ('Star Wars', 105, 'Set in a distant galaxy, the story follows Luke Skywalker, played by Mark Hamill, a young farm boy who becomes embroiled in the Rebel Alliances fight against the tyrannical Galactic Empire. Assisted by Jedi Master Obi-Wan Kenobi, the charismatic smuggler Han Solo, and the droids C-3PO and R2-D2, Luke sets out on a journey to rescue Princess Leia, portrayed by Carrie Fisher, and deliver the plans to the Death Star.', 'images/image3.jpg'),
+  ('Avengers', 95, 'The story follows the aftermath of "Avengers: Infinity War," where the villain Thanos successfully wiped out half of all life in the universe. In "Endgame," the surviving Avengers embark on a time-traveling mission to collect the Infinity Stones and undo Thanos devastating snap. The film explores themes of sacrifice, heroism, and the enduring strength of the human spirit.', 'images/image2.jpg'),
+  ('Thor', 140, 'The story follows Thor, the Norse god of thunder, who is banished from the realm of Asgard to Earth as punishment for his reckless behavior. Stripped of his powers and separated from his mystical hammer Mjolnir, Thor must learn humility and humanity. As he navigates life on Earth, he faces various challenges, including a romantic connection with scientist Jane Foster.', 'images/image4.jpg'),
+  ('Batman', 240, 'Directed by Christopher Nolan, "The Dark Knight" is the second installment in Nolans Batman trilogy. The film follows Bruce Wayne (Christian Bale) as Batman, who faces a new threat in the form of the Joker (Heath Ledger), a criminal mastermind with a penchant for chaos. As the Joker wreaks havoc on Gotham City, Batman grapples with ethical dilemmas and the limits of justice', 'images/image5.jpg'),
+  ('Avengers', 105, 'The story begins with the unassuming hobbit Frodo Baggins inheriting a powerful ring that could bring about the destruction of Middle-earth. Frodo sets out on a perilous journey to Mount Doom to destroy the ring, accompanied by a diverse fellowship representing different races.', 'images/image6.jpg');
+
 
 INSERT INTO USERS (username, password, admin) VALUES
   ('admin', 'adminpassword', 1),
@@ -68,7 +71,15 @@ INSERT INTO USERS (username, password, admin) VALUES
 INSERT INTO SEANCES (movie_id, hall_id, date, start_time) VALUES
   (1, 1, '2024-01-27', '18:00:00'),
   (2, 2, '2024-01-28', '19:30:00'),
-  (3, 3, '2024-01-29', '20:15:00');
+  (3, 3, '2024-01-29', '20:15:00'),
+  (1, 1, '2024-01-28', '16:30:00'),
+  (2, 2, '2024-01-30', '17:20:00'),
+  (3, 3, '2024-02-11', '18:40:00'),
+  (4, 1, '2024-02-12', '19:45:00'),
+  (5, 2, '2024-02-13', '20:35:00'),
+  (6, 3, '2024-02-14', '21:30:00'),
+  (6, 1, '2024-02-15', '19:30:00'),
+  (4, 2, '2024-02-16', '10:30:00');
 
 INSERT INTO RESERVATIONS (user_id, seance_id, seat_id) VALUES
   (2, 1, 5),
